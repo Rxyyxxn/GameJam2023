19257f5370649437d5c2cc6a52ac175577e64cc2
Rxyyxxn <rayyaanputra@gmail.com>
2023-10-12 08:42:25 +0800

    PlayerUI update

--- Assets/PlayerUI/PlayerUI.cs
+++ Assets/PlayerUI/PlayerUI.cs
@@ -1,15 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ShopManager : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
-    public string[] description;
-    public Image[] diceimages;
-    public Image diceimage;
-    public static ShopManager instance;
-
+    public static PlayerUI instance;
     private void Awake()
     {
         if (instance == null)
@@ -23,13 +18,15 @@ public class ShopManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        gameObject.SetActive(false);
+        
     }
 
-    public void D6()
+    // Update is called once per frame
+    void Update()
     {
-        diceimage = diceimages[1];
+        
     }
 }
