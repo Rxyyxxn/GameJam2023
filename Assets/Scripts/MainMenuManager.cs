f9f21e3b9964b24d701e57f3f3ac6518bd1f4767
TriaAri <ariwardenny23@gmail.com>
2023-10-13 12:15:28 +0800

    mainmenu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject tapToStartText;
+    Image image;
+
+    private void Awake()
+    {
+        image = tapToStartText.GetComponent<Image>();
+    }
     public void OnButtonChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        image.CrossFadeAlpha(0.5f, 5, false);
     }
 }
